3ae76ed63ae344121e9ddf5a16d0bb76aff89854
github-Artem-Freelancer <artemkostin.98@mail.com>
2022-06-24 15:21:45 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function reverse (n) {
-  
+	if (n < 0) {
+		n = n * (-1);
+		n = n.toString();
+		let arrayNumbers = Array.from(n);
+		arrayNumbers.reverse();
+		n = String(arrayNumbers);
+		n = n.replace(/,/g, "");
+		return n;
+	} else {
+		n = n.toString();
+		let arrayNumbers = Array.from(n);
+		arrayNumbers.reverse();
+		n = String(arrayNumbers);
+		n = n.replace(/,/g, "");
+		return n;
+	}
 }
